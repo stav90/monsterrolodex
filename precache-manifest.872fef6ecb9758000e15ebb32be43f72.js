145426fde079bb4af2135ddf7801ff00e0d6bab8
Daniel Stavrakis <>
2020-02-04 19:06:32 +1030

    Updates

---
+++ precache-manifest.872fef6ecb9758000e15ebb32be43f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2681ab2cccad4dd67a00fa4edd06ebf9",
+    "url": "/monsterrolodex/index.html"
+  },
+  {
+    "revision": "d42189e9750bc786248b",
+    "url": "/monsterrolodex/static/css/main.9bef2b13.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsterrolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsterrolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42189e9750bc786248b",
+    "url": "/monsterrolodex/static/js/main.b7753649.chunk.js"
+  },
+  {
+    "revision": "94be1468c95c56332ee5",
+    "url": "/monsterrolodex/static/js/runtime-main.b925d36d.js"
+  }
+]);
